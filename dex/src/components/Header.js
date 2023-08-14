73a1cf2f1480c3ffd2d8ce9ee9ab9dcd7675ed9d
saske <yteryur888991@gmail.com>
2023-08-14 23:03:03 +0300

    added connect wallet

--- dex/src/components/Header.js
+++ dex/src/components/Header.js
@@ -4,7 +4,12 @@ import ETH from '../eth.svg'
 import { Link } from 'react-router-dom'
 
 
-function Header() {
+function Header(props) {
+
+
+
+  const {address, isConnected, connect} = props;
+
   return (
     <header>
       <div className='leftH'>
@@ -21,8 +26,8 @@ function Header() {
           <img src={ETH} alt='eth' className='eth' />
           Ethereum
         </div>
-        <div className='connectButton'>
-          Connect
+        <div className='connectButton' onClick={connect}>
+           {isConnected ? (address.slice(0,4) + "...." +address.slice(38)) : "Connect"}
         </div>
       </div>
     </header>
